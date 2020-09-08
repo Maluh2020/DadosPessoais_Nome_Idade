4150471e407242cf50bfc1b559f4675873b30674
Maluh2020 <maluhmyth@gmail.com>
2020-09-07 22:36:06 -0300

    Projeto para saber qual a pessoa mais velha, digitando nome, idade e salário.

---
+++ ExercDadosPessoa/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace ExercDadosPessoa
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public double salario;
+
+       
+    }
+}
